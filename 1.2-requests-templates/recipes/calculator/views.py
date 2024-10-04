354a716e3aa33b6d5226985ca363c4164682297d
Андрей Сенников <sennikov.ai@corp.ttpk.ru>
2024-10-04 12:07:54 +1000

    ADD URL Recipe

--- 1.2-requests-templates/recipes/calculator/views.py
+++ 1.2-requests-templates/recipes/calculator/views.py
@@ -4,7 +4,7 @@ DATA = {
     'omlet': {
         'яйца, шт': 2,
         'молоко, л': 0.1,
-        'соль, ч.л.': 0.5,
+        'соль, ч.л': 0.5,
     },
     'pasta': {
         'макароны, г': 0.3,
@@ -16,15 +16,51 @@ DATA = {
         'сыр, ломтик': 1,
         'помидор, ломтик': 1,
     },
-    # можете добавить свои рецепты ;)
+    'strawberry': {
+        'шоколад, плитка': 1,
+        'сахар, ч.л': 2,
+        'сметана, ч.л': 4,
+        'клубника, шт': 5
+    },
+    'cutlets': {
+        'фарш, г': 200,
+        'рис, г': 50,
+        'мука, ст': 0.8,
+        'соль, ч.л.': 0.4
+    }
 }
 
-# Напишите ваш обработчик. Используйте DATA как источник данных
-# Результат - render(request, 'calculator/index.html', context)
-# В качестве контекста должен быть передан словарь с рецептом:
-# context = {
-#   'recipe': {
-#     'ингредиент1': количество1,
-#     'ингредиент2': количество2,
-#   }
-# }
+
+def calculate_recipe_view(request, recipe_name):
+
+    if recipe_name in DATA:
+        data = DATA[recipe_name]
+        servings = request.GET.get('servings', None)
+
+        if servings:
+            result = dict()
+            for item, value in data.items():
+                new_value = value * int(servings)
+                result[item] = new_value
+            context = {
+                'recipe_name': recipe_name,
+                'recipe': result
+            }
+        else:
+            context = {
+                'recipe_name': recipe_name,
+                'recipe': data
+            }
+
+    else:
+        context = None
+
+    return render(request, template_name='calculator/index.html', context=context)
+
+
+def home_view(request):
+
+    all_recipes = list(DATA.keys())
+    context = {'all_recipes': all_recipes}
+
+    return render(request, template_name='home/home.html', context=context)
